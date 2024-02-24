aa1392972df3f4ebae162c782ae0699ea6e7f543
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-24 08:09:14 +0530

    Accessing elements from DOM

--- Starter/2. Accessnig the DOM/live-coding/app.js
+++ Starter/2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,22 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
+console.log(document.getElementsByTagName("h1"));
+
 // 2. getElementById
+console.log(document.getElementById("main"));
+
 // 3. getElementsByClassName
+console.log(document.getElementsByClassName("cls"));
+
 // 4. querySelector
+console.log(document.querySelector(".cls"));
+console.log(document.querySelector("#main"));
+console.log(document.querySelector("li"));
+
 // 5. querySelectorAll
+console.log(document.querySelectorAll("li"));
 
 // ---------------------------------------
 // Storing data in variables
+const h1 = document.querySelector("h1");
+console.log(h1);
