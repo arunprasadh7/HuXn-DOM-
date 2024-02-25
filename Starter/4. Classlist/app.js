c9784a3fd51ca362cf604bcfa320f6f08063f080
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-25 12:24:45 +0530

    DOM classes

--- Starter/4. Classlist/app.js
+++ Starter/4. Classlist/app.js
@@ -1,4 +1,13 @@
+const h1 = document.querySelector("h1");
+
 // classlist
+console.log(h1.classList);
+
 // add()
+console.log(h1.classList.add("apple"));
+
 // remove()
+console.log(h1.classList.remove("apple"));
+
 // toggle()
+console.log(h1.classList.toggle("styles"));
