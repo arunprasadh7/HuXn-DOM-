9bde561cb29d0be586dd21b6e19c5a253b33fdf5
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-25 12:13:26 +0530

    3 All about text

--- Starter/3. innerText, textContent, innerHTML/exercise/app.js
+++ Starter/3. innerText, textContent, innerHTML/exercise/app.js
@@ -1,3 +1,11 @@
 // 1. Check the innerText of "first" element
+const one = document.querySelector(".first");
+console.log(one.innerText);
+
 // 2. Check the textContent of "second" element
+const two = document.querySelector(".second");
+console.log(two.textContent);
+
 // 3. Check the innerHTML of "third" element
+const three = document.querySelector(".third");
+console.log(three.innerHTML);
--- Starter/3. innerText, textContent, innerHTML/live coding/app.js
+++ Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,15 @@
 // innerText
+const p1 = document.querySelector("p");
+console.log(p1.innerText);
+
 // textContent
+const p2 = document.querySelector("p");
+console.log(p2.textContent);
+
 // innerHTML
+const p3 = document.querySelector("p");
+console.log(p3.innerHTML);
+
+// Changing html content
+const c1 = document.querySelector("h1");
+c1.innerHTML = "Arun";
