34c52d51244d10a0a480191ae0db33052354a0f4
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-26 03:18:53 +0530

    DOM attributes

--- Starter/5 Getting And Setting Attributes/exercise/app.js
+++ Starter/5 Getting And Setting Attributes/exercise/app.js
@@ -1,4 +1,12 @@
 // 1. Select the anchor tag
+const a = document.querySelector("a");
+
 // 2. Use getAttribute(attrName) to check the attribute.
+console.log(a.getAttribute("href"));
+
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+const two = document.querySelector(".a-two");
+two.setAttribute("href", "https://www.udemy.com/");
+console.log(two.href);
+two.setAttribute("target", "blank");
--- Starter/5 Getting And Setting Attributes/live coding/app.js
+++ Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -1,5 +1,28 @@
+const a = document.querySelector("a");
+const input = document.querySelector("input");
+
 // href
+console.log(a.href);
+console.log((a.href = "www.youtube.com"));
+
 // value
+// input.value = "Arun";
+console.log(input.value);
+
 // type
+const pass = document.querySelector("#pwd");
+pass.type = "password";
+pass.placeholder = "Please provide a strong password";
+
 // getAttribute(attrName)
+const mail = document.querySelector("#email");
+console.log(mail.getAttribute("type"));
+console.log(mail.getAttribute("placeholder"));
+
 // setAttribute(attrName, value)
+const uname = document.querySelector("#uname");
+uname.setAttribute("type", "text");
+uname.setAttribute("placeholder", "Enter username");
+
+console.log(uname.type);
+console.log(uname.placeholder);
