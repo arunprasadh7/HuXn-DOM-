da94d5e46246a43b3305f5940769909b9adcc63a
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-26 07:18:37 +0530

    DOM event basics

--- Starter/9. 3 ways to add events/app.js
+++ Starter/9. 3 ways to add events/app.js
@@ -1,5 +1,28 @@
 // ----------- BAD WAY-----------
+const secondBtn = document.querySelector(".second-btn");
+secondBtn.onclick = function () {
+  console.log("Arun Web Dev");
+};
 
 // ----------- GREAT WAY-----------
 
+const best = document.querySelector(".best");
+const bestFun = () => console.log("BEst Way to add events ");
+
+best.addEventListener("click", bestFun);
+
 // ----------- Event (e) Object -----------
+const p = document.querySelector(".para");
+const eve = (event) => console.log(event);
+
+p.addEventListener("click", eve);
+
+// form data
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const fevent = (event) => {
+  event.preventDefault();
+  console.log(input.value);
+};
+
+form.addEventListener("submit", fevent);
