6224b57e06d2d9da415f68714d04e1fd5165c164
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-02-26 10:46:43 +0530

    DOM Mouse & Keyboard Events

--- Starter/10. Events In-Depth/1. Mouse/app.js
+++ Starter/10. Events In-Depth/1. Mouse/app.js
@@ -12,10 +12,31 @@
 // 10. mouseup
 
 let one = document.querySelector(".click");
+const red = () => {
+  one.classList.toggle("red");
+};
+one.addEventListener("click", red);
+
 let two = document.querySelector(".contextmenu");
+two.addEventListener("contextmenu", () => {
+  two.classList.toggle("blue");
+});
+
 let three = document.querySelector(".dblclick");
+three.addEventListener("dblclick", () => {
+  three.classList.toggle("teal");
+});
+
 let four = document.querySelector(".mousedown");
+four.addEventListener("mousedown", () => {
+  four.classList.toggle("steelblue");
+});
+
 let five = document.querySelector(".mouseenter");
+five.addEventListener("mousedown", () => {
+  five.classList.toggle("orange");
+});
+
 let six = document.querySelector(".mouseleave");
 let seven = document.querySelector(".mousemove");
 let eight = document.querySelector(".mouseout");
